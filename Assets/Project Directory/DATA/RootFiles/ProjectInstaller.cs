671079e23cff2980c8e17ba07c3669ed1064d9e2
Dialmee <maggie.fabre02@gmail.com>
2025-08-28 15:59:55 +0200

    [DATA][GPP]ReflexAdded

---
+++ Assets/Project Directory/DATA/RootFiles/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+using Reflex.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Reflex.Core;
+using UnityEngine;
+
+public class ProjectInstaller : MonoBehaviour, IInstaller
+{
+    public void InstallBindings(ContainerBuilder builder)
+    {
+        /*builder.AddSingleton<SceneManager>();
+        builder.AddSingleton<SoundManager>();
+        builder.AddSingleton<LeaderboardServerBridge>();*/
+    }
+}
+
